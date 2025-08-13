0d79b3377c591fa2a8ef36497a273e93b555698b
Simon Jäger <simonjager@hotmail.com>
2025-08-13 10:54:01 +0200

    Modified menu, added project menu, added project class and subclasses

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 # Imports
 import sys
 
+class Project:
+    def __init__(self, name, start, finish, progress, status, language, link):
+        self.name = name
+        self.start = start
+        self.finish = finish
+        self.progress = progress
+        self.status = status
+        self.language = language
+        self.link = link
+
+class ProgrammingProject(Project):
+    def __init__(self, name, start, finish, progress, status, language, link):
+        super().__init__(name, start, finish, progress, status, language, link)
+        
+
+
+class EverydayProject(Project):
+    def __init__(self, name, start, finish, progress, status):
+        super().__init__(name, start, finish, progress, status)
+
+
 
 # Main function
 def main():
+    programming_projects = []
+    everyday_projects = []
+    programming_archive = []
+    everyday_archive = []
     start_menu()
 
 
 def start_menu():
     print("What would you like to do?\n")
-    print("1. Add new project")
-    print("2. View programming projects")
-    print("3. View everyday projects")
-    print("4. View archived projects")
-    print("5. Exit\n\n")
+    print("1. Add new project\n2. View programming projects\n3. View everyday projects\n4. View archived projects\n5. Exit program\n\n")
     validate_choice(str(input("Enter choice: ")))
 
 
 def validate_choice(choice):
     if choice == "1":
-        add_project()
+        add_project_menu()
     elif choice == "2":
         view_programming()
     elif choice == "3":
@@ -34,10 +55,35 @@ def validate_choice(choice):
         start_menu()
 
 
-def add_project():
-    print("Adding project")
+def add_project_menu():
+    print("Select project type:\n")
+    print("1. Programming project\2. Everyday project\n3. Return to main menu\n4. Exit program")
+    choice = str(input("Enter choice: "))
+    if choice == "1":
+        add_project_programming()
+    elif choice == "2":
+        add_project_everyday()
+    elif choice == "3":
+        start_menu()
+    elif choice == "4":
+        exit()
 
 
+
+def add_project_programming():
+    project = ProgrammingProject()
+    
+
+
+def add_project_everyday():
+    project = EverydayProject()
+
+
+def view_archive():
+    print("Select which archive to view:\n")
+    print("1. Programming projects\n2. Everyday projects\n")
+
+    
 def view_programming():
     print("View programming projects")
 
@@ -46,8 +92,8 @@ def view_everyday():
     print("View everyday projects")
 
 
-def view_archive():
-    print("View project archive")
+
+    
 
 
 def exit():
@@ -59,6 +105,11 @@ def exit():
     else:
         print("Invalid option, type 'Y' for yes or 'N for no")
         exit()
+
+
+
+
+
 
 
 # Execute main
