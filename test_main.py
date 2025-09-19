59b0548bc120825d6cccb78f05638d078c91273e
Simon Jäger <simonjager@hotmail.com>
2025-09-19 15:20:29 +0200

    latest update

--- test_main.py
+++ test_main.py
@@ -3,24 +3,9 @@ import pytest
 import sys
 
 from .main import add_project_menu, programming_projects, ProgrammingProject, everyday_projects, EverydayProject, Project, exit
-
-'''
-@pytest.fixture
-def project_manager():
-    return Project
-
-@pytest.fixture
-def programming_manager():
-    return ProgrammingProject
-
-@pytest.fixture
-def everyday_manager():
-    return EverydayProject
 '''
 def test_add_project_menu():
-    ...
-    # print(add_project_menu)
-    # assert add_project_menu(choice = "1") == ProgrammingProject.add_project_programming(programming_projects)
+    assert add_project_menu().choice("1") == ProgrammingProject.add_project_programming(programming_projects)
 
 def test_add_programming_project(programming_manager):
     ...
@@ -31,6 +16,11 @@ def test_add_everyday_project(everyday_manager):
 def test_view_projects():
     ...
 
-def test_exit():
-    assert exit() == sys.exit()
-# test_add_project_menu()
+@pytest.fixture
+def prompt():
+    return "y"
+'''
+
+def test_exit(monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda _: "y")
+    assert exit() == sys.exit("\nExiting program")
