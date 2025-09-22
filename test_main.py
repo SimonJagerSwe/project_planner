2e5b4f5ea408ff638cc38f30de154a0b3a94f7af
Simon Jäger <simonjager@hotmail.com>
2025-09-22 15:43:33 +0200

    several successful tests added

--- test_main.py
+++ test_main.py
@@ -3,14 +3,8 @@ import mock
 import pytest
 import sys
 
-from .main import exit
-
-def test_add_project_menu():
-    ...
-    # assert add_project_menu().choice("1") == ProgrammingProject.add_project_programming(programming_projects)
-
-def test_add_programming_project():
-    mock_pp = {
+from .main import main, initialize_project_lists, ProgrammingProject, add_project_menu, programming_projects, exit
+mock_pp = {
         "name" : "Mock program 1",
         "start" : "1970-01-01",
         "finish" : "2055-12-31",
@@ -19,10 +13,8 @@ def test_add_programming_project():
         "progress" : "99.9%",
         "status" : "Eternal"
     }
-    mock_pl = []
-
-def test_add_everyday_project():
-    mock_ep = {
+mock_pl = []
+mock_ep = {
         "name" : "Mock everyday 1",
         "start" : "1970-01-01",
         "finish" : "2055-12-31",
@@ -30,18 +22,33 @@ def test_add_everyday_project():
         "status" : "Eternal"
 
     }
-    mock_el = []
+mock_el = []
+
+def test_main_init():
+    assert main() == initialize_project_lists()
+
+def test_add_project_menu(monkeypatch):
+    ...
+    # monkeypatch.setattr("builtins.input", lambda _: "1")
+    # assert add_project_menu() == ProgrammingProject.add_project_programming(programming_projects)
+
+def test_add_programming_project():
+    ...
+
+def test_add_everyday_project():
+    ...
 
 def test_view_projects():
     ...
 
-def test_exit(monkeypatch):
+def test_exit_upper(monkeypatch):
     # Check lowercase y exits program
     monkeypatch.setattr("builtins.input", lambda _: "y")
     with pytest.raises(SystemExit) as input:
         exit()
     assert input.type == SystemExit
 
+def test_exit_lower(monkeypatch):
     # Check uppercase Y converts to lowercase and exits program
     monkeypatch.setattr("builtins.input", lambda _: "Y")
     with pytest.raises(SystemExit) as input:
